f396c140b89f83065e79d9fc2d0892dc76cd3bd6
Lucas Santi <idlucassanti@hotmail.com>
2024-06-30 02:25:06 -0300

    Configuração da Fonte

--- App.tsx
+++ App.tsx
@@ -1,7 +1,20 @@
 import { StatusBar } from 'react-native';
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
 import { Home } from './src/Home';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold
+  });
+
+  if(!fontsLoaded) {
+    return <Loading />
+  }
+
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
